e35ffd4a48c9b863ed4062f95c9350c249f57513
Austin Polk <117412464+ApeZilla3@users.noreply.github.com>
2024-04-30 16:44:05 -0500

    Add files via upload

---
+++ sensor_new_v2.ino
@@ -0,0 +1,125 @@
+#include <Wire.h>
+#include <ArduinoBLE.h> 
+//#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM6DS3TRC.h>
+
+#define dataNum 14
+
+Adafruit_LSM6DS3TRC mpu;
+
+uint8_t readData;                   // for reading IMU register
+uint32_t timestamp;                 // for delay function
+union unionData {                   //Union for bit convertion 16 <--> 8
+  int16_t   dataBuff16[dataNum/2];
+  uint8_t   dataBuff8[dataNum];
+};
+union unionData ud;
+
+#define myUUID(val) ("0dd7eb5a-" val "-4f45-bcd7-94c674c3b25f")
+BLEService        AttService(myUUID("0000"));
+BLECharacteristic dataCharacteristic(myUUID("0010"), BLERead | BLENotify, dataNum);
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin_I2C()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  // mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  // mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  // mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+    if (!BLE.begin()) {
+    Serial.println("starting BLE module failed!");
+    while (1);
+  }
+
+    // set the local name
+  BLE.setLocalName("Att_Monitor");
+  // set the device name
+  BLE.setDeviceName("XIAO nRF52840 Sence");
+  // set the UUID for the service
+  BLE.setAdvertisedService(AttService);
+  // add the characteristic to the service  
+  AttService.addCharacteristic(dataCharacteristic);
+  // add service  
+  BLE.addService(AttService);
+
+    // start advertising
+  BLE.setAdvertisingInterval(160);    //0.625mS * 160 = 100mS
+  BLE.setConnectionInterval(6, 3200); //1.25mS * 6 = 7.5mS, 1.25mS * 3200 = 4S
+  BLE.advertise();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+    // connect the sentral
+  Serial.println("Connecting to Central ........");
+  BLEDevice central = BLE.central();
+
+  if(central){
+    Serial.print("Base System Found");
+
+    uint32_t time = millis();
+
+    while(central.connected()){
+
+      mpu.getEvent(&a, &g, &temp);
+
+       //Converting float data to uint16_t data
+    ud.dataBuff16[0] = (int16_t)(a.acceleration.x * 128);
+    ud.dataBuff16[1] = (int16_t)(a.acceleration.y * 128);
+    ud.dataBuff16[2] = (int16_t)(a.acceleration.z * 128);
+    ud.dataBuff16[3] = (int16_t)(g.gyro.x * 128);
+    ud.dataBuff16[4] = (int16_t)(g.gyro.y * 128);
+    ud.dataBuff16[5] = (int16_t)(g.gyro.z * 128);
+    ud.dataBuff16[6] = (int16_t)(temp.temperature * 128);
+    
+    if(millis() - 500 > time){
+      /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+  delay(500);
+     }
+           // write to Characteristic as byte data        
+      dataCharacteristic.writeValue(ud.dataBuff8, dataNum);
+
+  }
+}
+
+  
+}
